9ca918889cf7597b469c151939ad3f16306f60ab
seem <wasim@dataprophet.com>
2019-12-01 21:03:23 +0200

    Day 01

---
+++ day01.py
@@ -0,0 +1,31 @@
+def get_fuel(mass: int) -> int:
+    return mass // 3 - 2
+
+
+assert get_fuel(12) == 2
+assert get_fuel(14) == 2
+assert get_fuel(1969) == 654
+assert get_fuel(100756) == 33583
+
+with open("data/day01.txt") as f:
+    masses = [int(line) for line in f.read().strip().split("\n")]
+
+total_fuel = sum(get_fuel(mass) for mass in masses)
+print(total_fuel)
+
+
+def get_recursive_fuel(mass: int) -> int:
+    fuel = max(0, mass // 3 - 2)
+    if fuel == 0:
+        return fuel
+    else:
+        return fuel + get_recursive_fuel(fuel)
+
+
+assert get_recursive_fuel(2) == 0
+assert get_recursive_fuel(14) == 2
+assert get_recursive_fuel(1969) == 966
+assert get_recursive_fuel(100756) == 50346
+
+total_recursive_fuel = sum(get_recursive_fuel(mass) for mass in masses)
+print(total_recursive_fuel)
